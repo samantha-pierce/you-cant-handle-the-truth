694d5aec9956e4812de5f7f22c83f0d801cae223
Samantha-Pierce <spapierce@gmail.com>
2020-11-21 13:53:01 -0800

    added test case with lower and uppercase words and added fix

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -13,7 +13,8 @@ public class HandleTruthTest {
                 "",
                 "one",
                 "how many words are any words duplicate",
-                "son we live in a world that has walls and those walls have to be guarded by men with guns whos gonna do it you you lieutenant weinberg i have a greater responsibility than you can possibly fathom you weep for santiago and you curse the marines you have that luxury you have the luxury of not knowing what i know that santiagos death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives you dont want the truth because deep down in places you dont talk about at parties you want me on that wall you need me on that wall we use words like honor code loyalty we use these words as the backbone of a life spent defending something you use them as a punchline i have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that i provide and then questions the manner in which i provide it i would rather you just said thank you and went on your way otherwise i suggest you pick up a weapon and stand a post either way i dont give a damn what you think you are entitled to"
+                "son we live in a world that has walls and those walls have to be guarded by men with guns whos gonna do it you you lieutenant weinberg i have a greater responsibility than you can possibly fathom you weep for santiago and you curse the marines you have that luxury you have the luxury of not knowing what i know that santiagos death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives you dont want the truth because deep down in places you dont talk about at parties you want me on that wall you need me on that wall we use words like honor code loyalty we use these words as the backbone of a life spent defending something you use them as a punchline i have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that i provide and then questions the manner in which i provide it i would rather you just said thank you and went on your way otherwise i suggest you pick up a weapon and stand a post either way i dont give a damn what you think you are entitled to",
+                "Jack and Ben waited for the bus at the Bus Station"
         };
 
         List<TreeMap<Integer, Set<String>>> testCases = getTreeMapTestCases();
@@ -44,9 +45,7 @@ public class HandleTruthTest {
         TreeMap<Integer, Set<String>> map2 = new TreeMap<>(Collections.reverseOrder());
         HashSet<String> map2Set = new HashSet<>();
         String[] arr = new String[]{"how", "are", "duplicate", "many", "any"};
-        for (String s : arr) {
-            map2Set.add(s);
-        }
+        map2Set.addAll(Arrays.asList(arr));
         map2.put(2, Collections.singleton("words"));
         map2.put(1, map2Set);
         testCases.add(map2);
@@ -55,29 +54,15 @@ public class HandleTruthTest {
         TreeMap<Integer, Set<String>> map3 = new TreeMap<>(Collections.reverseOrder());
         HashSet<String> map3Set1 = new HashSet<>();
         String[] set1 = new String[]{"a", "and", "i"};
-        for (String s : set1) {
-            map3Set1.add(s);
-        }
+        map3Set1.addAll(Arrays.asList(set1));
         HashSet<String> map3Set2 = new HashSet<>();
-        String[] set2 = new String[]{"have", "to"};
-        for (String s : set2) {
-            map3Set2.add(s);
-        }
+        map3Set2.addAll(Arrays.asList(new String[]{"have", "to"}));
         HashSet<String> map3Set3 = new HashSet<>();
-        String[] set3 = new String[]{"in", "use", "of", "dont", "we", "on"};
-        for (String s : set3) {
-            map3Set3.add(s);
-        }
+        map3Set3.addAll(Arrays.asList(new String[]{"in", "use", "of", "dont", "we", "on"}));
         HashSet<String> map3Set4 = new HashSet<>();
-        String[] set4 = new String[]{"lives", "want", "words", "it", "while", "way", "as", "walls", "what", "provide", "me", "luxury", "wall"};
-        for (String s : set4) {
-            map3Set4.add(s);
-        }
+        map3Set4.addAll(Arrays.asList(new String[]{"lives", "want", "words", "it", "while", "way", "as", "walls", "what", "provide", "me", "luxury", "wall"}));
         HashSet<String> map3Set5 = new HashSet<>();
-        String[] set5 = new String[]{"explain", "very", "guns", "saved", "lieutenant", "about", "your", "these", "saves", "would", "pick", "fathom", "grotesque", "incomprehensible", "because", "greater", "give", "sleeps", "went", "loyalty", "myself", "them", "then", "something", "at", "knowing", "santiago", "curse", "weinberg", "entitled", "whos", "be", "probably", "world", "freedom", "responsibility", "are", "by", "tragic", "backbone", "man", "stand", "inclination", "think", "gonna", "suggest", "places", "thank", "under", "punchline", "honor", "existence", "defending", "questions", "do", "manner", "down", "either", "son", "post", "than", "talk", "has", "up", "those", "possibly", "which", "otherwise", "need", "like", "my", "parties", "know", "santiagos", "rises", "who", "deep", "code", "death", "rather", "for", "weep", "life", "nor", "guarded", "can", "weapon", "not", "men", "said", "just", "live", "spent", "damn", "with", "truth", "marines", "neither", "time", "blanket"};
-        for (String s : set5) {
-            map3Set5.add(s);
-        }
+        map3Set5.addAll(Arrays.asList(new String[]{"explain", "very", "guns", "saved", "lieutenant", "about", "your", "these", "saves", "would", "pick", "fathom", "grotesque", "incomprehensible", "because", "greater", "give", "sleeps", "went", "loyalty", "myself", "them", "then", "something", "at", "knowing", "santiago", "curse", "weinberg", "entitled", "whos", "be", "probably", "world", "freedom", "responsibility", "are", "by", "tragic", "backbone", "man", "stand", "inclination", "think", "gonna", "suggest", "places", "thank", "under", "punchline", "honor", "existence", "defending", "questions", "do", "manner", "down", "either", "son", "post", "than", "talk", "has", "up", "those", "possibly", "which", "otherwise", "need", "like", "my", "parties", "know", "santiagos", "rises", "who", "deep", "code", "death", "rather", "for", "weep", "life", "nor", "guarded", "can", "weapon", "not", "men", "said", "just", "live", "spent", "damn", "with", "truth", "marines", "neither", "time", "blanket"}));
         map3.put(18, Collections.singleton("you"));
         map3.put(9, Collections.singleton("the"));
         map3.put(8, map3Set1);
@@ -87,6 +72,16 @@ public class HandleTruthTest {
         map3.put(2, map3Set4);
         map3.put(1, map3Set5);
         testCases.add(map3);
+
+        // test case 4
+        TreeMap<Integer, Set<String>> map4 = new TreeMap<>(Collections.reverseOrder());
+        HashSet<String> map4Set1 = new HashSet<>();
+        map4Set1.addAll(Arrays.asList(new String[]{"bus", "the"}));
+        HashSet<String> map4Set2 = new HashSet<>();
+        map4Set2.addAll(Arrays.asList(new String[]{"jack", "and", "ben", "waited", "for", "at", "station"}));
+        map4.put(2, map4Set1);
+        map4.put(1, map4Set2);
+        testCases.add(map4);
 
         return testCases;
     };
